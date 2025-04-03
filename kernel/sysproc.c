018555f3952779f0e285757e3132e7ee6c20aaba
张海龙 <zhanghailong6214@163.com>
2025-04-03 14:22:53 +0800

    lab2

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,7 +6,10 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
+uint64 acquire_freemem();
+uint64 acquire_nproc();
 uint64
 sys_exit(void)
 {
@@ -104,5 +107,22 @@ sys_trace(void)
     return -1;
   struct proc *p = myproc();
   p->trace_mask = mask;
+  return 0;
+}
+
+uint64 sys_sysinfo(void){
+  struct sysinfo info;
+  uint64 addr;
+  struct proc *p = myproc();
+
+  info.nproc = acquire_nproc();
+  info.freemem = acquire_freemem();  
+
+  if(argaddr(0, &addr) < 0)
+    return -1;
+
+  if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+   
   return 0;
 }
