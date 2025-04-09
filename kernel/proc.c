5a923a825f37b8cee31dda709bcd70f1b567dcd0
张海龙 <zhanghailong6214@163.com>
2025-04-09 12:38:16 +0800

    lab4

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,7 +126,9 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
-
+  
+  p->ticks = 0;
+  p->handle_executing = 0;
   return p;
 }
 
